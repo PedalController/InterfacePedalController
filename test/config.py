b4c1821673b5facea9b23f1c74904d915b2c2e51
SrMouraSilva <mateus.moura@hotmail.com>
2016-11-02 10:31:20 -0300

    Fixed test

---
+++ test/config.py
@@ -0,0 +1,18 @@
+import sys
+import tornado
+
+sys.path.append('application')
+sys.path.append('webservice')
+
+from application.Application import Application
+from webservice.WebService import WebService
+
+address = 'localhost'
+port = 3000
+
+application = Application(data_patch="application/test/data/", address=address, test=True)
+application.register(WebService(application, port))
+
+application.start()
+
+tornado.ioloop.IOLoop.current().start()
